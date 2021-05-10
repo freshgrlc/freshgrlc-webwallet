b79f8f8d39df96c9f3c75a0d1ae02768cf0c526d
Kyle Rosenberg <kyle@ekrosenberg.com>
2021-05-10 19:30:29 -0400

    remove the trailing slash

--- src/apis/endpoints.ts
+++ src/apis/endpoints.ts
@@ -1,2 +1,2 @@
-// export const webwallet = 'https://api.freshgrlc.net/webwallet/';
-export const webwallet = 'http://localhost:8080/';
+// export const webwallet = 'https://api.freshgrlc.net/webwallet';
+export const webwallet = 'http://localhost:8080';
